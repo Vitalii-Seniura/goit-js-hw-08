7784576f5e9601863e73c72fe6fd1ad0642e4ef1
Vitalii-Seniura <senyura.w@gmail.com>
2022-06-14 00:19:02 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+
+import SimpleLightbox from "simplelightbox/";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = galleryItems.map((
+    { preview, original, description }) =>
+    `<a class="gallery__item" href='${original}'>
+    <img class='gallery__image' src='${preview}' 
+    alt='${description}' data-caption='${description}'>
+    </a>`).join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+gallery.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+};
+
+
+
